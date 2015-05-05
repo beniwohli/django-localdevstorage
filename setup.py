ef3674cf52e257e7483ce0da05d98e0b532ef149
Benjamin Wohlwend <piquadrat@gmail.com>
2015-05-05 20:53:26 +0200

    explicitly define Python 3.3/3.4 support, add support for building universal wheels

--- setup.py
+++ setup.py
@@ -47,9 +47,10 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Topic :: Software Development",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ]
